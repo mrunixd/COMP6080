77cfa6fabb118047f02cdec910023ed72a213170
c-ientific <z5481212@ad.unsw.edu.au>
2024-09-03 13:46:06 +1000

    working on exercises 2 lab04

--- labs/lab04/exercise2/src/App.js
+++ labs/lab04/exercise2/src/App.js
@@ -1,43 +1,71 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="container">
-        <h1>2048</h1>
-        <table border="1">
-          <tr>
-            <td>
-              <div className="cell"id="two">
-                <p>2</p>
-              </div>
-            </td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-        </table> 
-      </div>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container">
+				<h1>2048</h1>
+				<Grid />
+			</div>
+		</div>
+	);
+}
+
+function Number({ value })  {
+	return <div class="cell" id="two">{value}</div>
+}
+
+function Cell({ number }) {
+	if (number) {
+		return <td><Number value={number}/></td>
+	}
+	return <td></td>
+}
+
+function Grid() {
+	const [cells, setCells] = useState(Array(16).fill(2, 0, 1).fill(null, 1, 16));
+	
+	const handleKeyDown = (event) => {
+		if (event.key === "w") {
+
+		} else if (event.key === "a") {
+
+		} else if (event.key === "s") {
+
+		} else if (event.key === "d") {
+
+		}
+	}
+	
+	return  (
+		<table border="1" onKeyDown={handleKeyDown}>
+			<tr>
+				<Cell number={cells[0]}/>
+				<Cell number={cells[1]}/>
+				<Cell number={cells[2]}/>
+				<Cell number={cells[3]}/>
+			</tr>
+			<tr>
+				<Cell number={cells[4]}/>
+				<Cell number={cells[5]}/>
+				<Cell number={cells[6]}/>
+				<Cell number={cells[7]}/>
+			</tr>
+			<tr>
+				<Cell number={cells[8]}/>
+				<Cell number={cells[9]}/>
+				<Cell number={cells[10]}/>
+				<Cell number={cells[11]}/>
+			</tr>
+			<tr>
+				<Cell number={cells[12]}/>
+				<Cell number={cells[13]}/>
+				<Cell number={cells[14]}/>
+				<Cell number={cells[15]}/>
+			</tr>
+		</table>
+	);
 }
 
 export default App;
